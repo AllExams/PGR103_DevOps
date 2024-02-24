6a9ed8557ebde23cdc08dc0f16fd049dc3a3d6a7
Langeland1996 <Studietfredrik@gmail.com>
2024-02-24 18:50:21 +0100

    updated main.yml for building and publishing docker image

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,12 @@
 variable "docker_username" {
-
+  description = "Docker Hub brukernavn"
 }
 
 variable "docker_password" {
-
+  description = "Docker Hub passord"
+  sensitive   = true
 }
 
 variable "repository_name" {
+  description = "Navn på Docker Hub-repository"
+}
